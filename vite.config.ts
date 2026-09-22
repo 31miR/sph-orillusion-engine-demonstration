7bf514d95ed71a9cf42b85e9b0978f9c0859c30b
31miR <76799355+31miR@users.noreply.github.com>
2026-09-22 21:48:14 +0200

    add workflow for deploying a dist to a github page

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+// This file has been defined purely so that github pages deployment works normally.
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '/sph-orillusion-engine-demonstration/',
+})
